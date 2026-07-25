66df3621ed621dfd4a9d7965d4a12d0825192853
ojbkxc <1657642978@qq.com>
2026-07-25 13:09:02 +0800

    Create fix_dup.py

---
+++ fix_dup.py
@@ -0,0 +1,10 @@
+﻿with open("server/Cargo.toml","r") as f: lines=f.readlines()
+found=False
+new_lines=[]
+for l in lines:
+ if l.strip()=="bytes = \"1\"":
+  if not found: found=True; new_lines.append(l)
+  else: print("Removed dup")
+ else: new_lines.append(l)
+open("server/Cargo.toml","w").writelines(new_lines)
+print("Done")
